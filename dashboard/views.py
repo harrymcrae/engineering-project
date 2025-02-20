39991531e8a8f1040fe79f3d39541d561aa32d3e
JeTTzx <dh671@exeter.ac.uk>
2025-02-20 18:29:01 +0000

    Adding Registration Page and Start of Dashboard Page
    Adding Fully Functional Registration page that matches the theme of the login page. Also adding a start layout of the dashboard (which is in early development)

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,3 +4,6 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def dashboard_page(request):
     return render(request, 'dashboard.html')
+
+def profile_view(request):
+    return render(request, 'dashboard.html', {'username': request.user.username})
