b0487c49ba0219a3f95ba81f3ec6451a5a87a6a2
JeTTzx <dh671@exeter.ac.uk>
2025-02-21 22:01:07 +0000

    Implemented the Design of the Dashboard and Slight Changes to Register System
    Updated the dashboard side menu, so it now includes a leader board button, as well as the rest of the buttons now are fully functional (map and rewards don't work as the map and rewards page doesn't exist yet).
    
    Implemented 3 panels for the dashboard which will be used to track the users progress in a simple environment.
    
    Also updated the register system, so once registered it automatically logs you in and takes you to the dashboard.

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import  login
+from .models import UserProfile
 
 def authView(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+
+            if not hasattr(user, 'profile'):
+                UserProfile.objects.create(user=user)
+
+            login(request, user)
+            return redirect('dashboard')
     else:
         form = UserCreationForm()
+        
     return render(request, 'registration/register.html', {'form': form})
