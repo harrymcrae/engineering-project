b4e812ae8825230f002fa78a2e09878a0bee8419
harrymcrae <hmcrae10@outlook.com>
2025-02-24 17:13:40 +0000

    Testing all features work, fixed location-based challenges

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,4 +4,4 @@ from .models import UserProfile
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'points') 
-    filter_horizontal = ('quizzes_completed',)
+    filter_horizontal = ('quizzes_completed', 'challenges_completed',)
