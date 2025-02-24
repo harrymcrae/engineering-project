769d31e0ff384b7512c316ca5248d40f3eb984a5
harrymcrae <hmcrae10@outlook.com>
2025-02-24 14:15:10 +0000

    Created a bonus model, made some minor tweaks

--- challenges/admin.py
+++ challenges/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Challenge, Quiz
+from .models import Challenge, Quiz, Bonus
 
 admin.site.register(Challenge)
 admin.site.register(Quiz)
+admin.site.register(Bonus)
--- challenges/models.py
+++ challenges/models.py
@@ -15,6 +15,20 @@ class Challenge(models.Model):
         verbose_name = "Challenge"
         verbose_name_plural = "Challenges"
 
+class Bonus(models.Model):
+    bonus_name = models.CharField(max_length=50,null=True)
+    bonus_description = models.CharField(max_length=255,null=True)
+    bonus_id = models.CharField(max_length=30, null=True)
+    points_awarded = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.bonus_name}"
+
+    class Meta:
+        verbose_name = "Bonus"
+        verbose_name_plural = "Bonuses"
+
+
 class Quiz(models.Model):
     quiz_name = models.CharField(max_length=50,null=True)
     quiz_description = models.CharField(max_length=255,null=True)
